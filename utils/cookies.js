d6c9a7b0961ae1523faa48f2d3f8c3af24c7c9ea
manis-h <manishabc83@gmail.com>
2023-04-03 13:23:12 +0530

    ok

--- utils/cookies.js
+++ utils/cookies.js
@@ -4,7 +4,9 @@ export const setCookie=(user,res,message,statusCode=200)=>{
     const token =jwt.sign({_id:user._id},"afadakjdakj")
     res.status(statusCode).cookie("token",token,{
             httpOnly:true,
-            maxAge:10*60*1000
+            maxAge:10*60*1000,
+            sameSite:"lax",
+            secure:false
     }).json({
         success: true,
         message,
