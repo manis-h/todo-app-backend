0bf35ad90849ab109134cc9631ac691aac10e866
manis-h <manishabc83@gmail.com>
2023-04-05 10:53:33 +0530

    production

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app =express();
 
 app.use(cookieParser())
 app.use(cors({
-    origin:'http://127.0.0.1:5173',
+    origin:'https://task-manager-by-manish.netlify.app/',
     credentials:true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     
